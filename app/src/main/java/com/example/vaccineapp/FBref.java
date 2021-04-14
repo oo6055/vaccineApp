fbdfe8a7e1368c9b303423080db942b3db3be4bd
Ori Ofek <oriofek106@gmail.com>
2021-04-14 13:53:06 +0300

    z

--- app/src/main/java/com/example/vaccineapp/FBref.java
+++ app/src/main/java/com/example/vaccineapp/FBref.java
@@ -2,7 +2,7 @@ package com.example.vaccineapp;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+ 
 public class FBref {
 
     public static FirebaseDatabase FBDB = FirebaseDatabase.getInstance();
